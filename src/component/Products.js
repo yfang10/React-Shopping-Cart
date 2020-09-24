e11aea61ba6b416f0850c23f7a17ec3099608e64
AndrewFyy <Yue930315@gmail.com>
2020-09-24 15:14:26 +0800

    Update Products.js

--- src/component/Products.js
+++ src/component/Products.js
@@ -6,7 +6,7 @@ export default class Products extends Component {
             <div>
                 <ul className = "products">
                     {this.props.products.map(product =>(
-                        <li key = {products._id}>
+                        <li key = {product._id}>
                             <div className = "product">
                                 <a href = {"#" + product._id}> 
                                 <img src = {product.image} alt = {product.title}></img>
