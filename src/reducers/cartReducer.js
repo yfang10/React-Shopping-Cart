5923b860ea0a3947b9016d0ad0947b8a9b08a5a4
AndrewFyy <Yue930315@gmail.com>
2020-09-28 14:58:31 +0800

    Update cartReducer.js

--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,13 +1,18 @@
 import {ADD_To_CART, REMOVE_FROM_CART} from '../types';
 
 export const cartReducer = (
-    
-)
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+    state = {cartItem: JSON.parse(localStorage.getItem("cartItems") || [""])},
+    action 
+) => {
+    switch(action.type){
+        case ADD_To_CART:
+            return{cartItem: action.payload.cartItems};
+
+        case REMOVE_TO_CART:
+            return{cartItem: action.payload.cartItems};
+        
+        default: 
+             return state;
+
     }
-}
+};
