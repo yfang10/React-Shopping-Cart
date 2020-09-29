c0361ca081fb666ebd3ee5a9656540019b84ef6d
AndrewFyy <Yue930315@gmail.com>
2020-09-29 11:31:26 +0800

    Update productActions.js

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -4,5 +4,21 @@ import {FILTER_PRODUCTS_BY_SIZE, ORDER_PRODUCTS_BY_PRICE} from "../types";
 export const fetchProducts = () => async(dispatch) => {
     const res = await fetch("/api/products");
     const data = await res.json();
+    console.log(data);
+    dispatch({
+        type: FETCH_PRODUCTS,
+        payload: data,
 
+    });
+};
+
+export const filterProducts = (products, size) => (dispatch) =>{
+    dispatch({
+        type: FILTER_PRODUCTS_BY_SIZE,
+        payload:{
+            size: size,
+            items:
+            size === ""
+        }
+    })
 }
