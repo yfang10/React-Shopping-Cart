f2e3fa2cd550596f47f1b82bfbc055845eae8890
AndrewFyy <Yue930315@gmail.com>
2020-09-30 09:36:15 +0800

    Update productActions.js

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -30,6 +30,22 @@ export const sortProducts = (filteredProducts, sort) => (dispatch) =>{
     const sortedProducts = filteredProducts.slice();
     if(sort === "latest"){
         sortedProducts.sort((a, b) => (a._id > b._id ? 1 : -1));
+    }else{
+        sortedProducts.sort((a, b) => 
+        sort === "lowest"
+        ? a.price > b.price
+        ? -1
+        : 1
+        );
     }
+    console.log(sortedProducts);
+dispatch({
+    type: ORDER_PRODUCTS_BY_PRICE,
+    payload:{
+        sort: sort,
+        items: sortedProducts
+    },
+});
+};
 
 }
