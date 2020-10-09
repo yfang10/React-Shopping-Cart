5c1d02b67f82982c16afd1a2623218085bce3cb9
AndrewFyy <Yue930315@gmail.com>
2020-10-09 17:18:21 +0800

    Update orderActions.js

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -5,6 +5,27 @@ export const createOrder = (order) => (dispatch) => {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-        }
+        },
+        body: JSON.stringify(order),
+
+
     })
-}
+    .then((res) => res.json())
+    .then((data) => {
+        dispatch({type: CREATE_ORDER, payload: data});
+        localStorage.clear("cartItems");
+        dispatch({type: CLEAR_CART});
+    });
+};
+
+export const clearOrder = () => (dispatch) => {
+    dispatch({type: CLEAR_ORDER});
+};
+
+export const fetchOrders = () => (dispatch) =>{
+    fetch("/api/orders")
+    .then((res) => res.json())
+    .then((data) => {
+        dispatch({type: FETCH_OFDERS, payload: data});
+    });
+};
