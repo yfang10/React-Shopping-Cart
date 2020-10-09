512909f064eeaf5753096f7b7df115af43d25ad8
AndrewFyy <Yue930315@gmail.com>
2020-10-09 17:33:08 +0800

    Update Cart.js

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,3 +2,11 @@ import React, {Component} from "react";
 import formatCurrency from "../util";
 import Fade from "react-reveal/Fade";
 import {connect} from "react-redux";
+import Modal from "react-modal";
+import Zoom from "react-reveal/Zoom";
+import {removeFromCart} from "../actions/cartActions";
+import {createOrders, clearOrder} from "../actions/orderActions";
+
+class Cart extends Component{
+    
+}
