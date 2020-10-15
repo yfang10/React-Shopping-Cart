db52d50b7ae342a68c96ab52f6ee426d4faeac7d
AndrewFyy <Yue930315@gmail.com>
2020-10-15 14:45:26 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,74 +8,26 @@ import Filter from './components/Filter';
 
 
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      products: data.products,
-      size: "",
-      sort: "",
-    };
-  }
+ render(){
+   return(
+     <Provider store = {store}>
+       <BrowserRouter>
+       <div className = "grid-container">
+         <header>
+           <Link to = "/">React Shopping Cart</Link>
+           <Link to = "/admin">Admin</Link>
+         </header>
+         <main>
+           <Route path = "/admin" component = {AdminScreen}/>
+           <Route path = "/" component = {HomeScreen} exact/>
 
-  sortProducts = (event) => {
-
-    const sort = event.target.value
-    console.log(event.target.value);
-    this.setState((state) => ({
-      sort: sort,
-      products: this.state.products.slice().sort((a, b) =>
-        sort === "lowest"
-        ?a.price > b.price
-        ? 1
-        :-1
-        :sort === "highest"
-        ?a.price < b.price
-        ? 1
-        :-1
-        :a._id > b._id
-        ? 1
-        :-1
-        ),
-    }));
-
-  };
-
-  filterProducts = (event) => {
-    console.log(event.target.value);
-    if(event.target.value === ""){
-      this.setState({size, product: data.products});
-    }else{
-    this.setState({
-      size: event.target.value,
-      products: data.products.filter((product) => product.availableSizes.indexOf(event.target.value) >= 0),
-
-    });
-  }
-};
-  render(){
-  return (
-    <div className = "grid-container">
-      <header>
-        <a href = "/">React Shopping Cart</a>
-              </header>
-              <main>
-                <div className = "content">
-                  <div className = "main">
-                    <Filter count = {this.state.products.length}>
-                      size = {this.state.size}
-                    </Filter>
-
-                 <Products products = {this.state.products}></Products>
-                  </div>
-                  <div className = "sidebar">Cart Items</div>
-                </div>        
-              </main>
-              <footer>
-                All right is reserved
-              </footer>
-    </div>
-  );
-  }
+         </main>
+         <footer>All right is reserved.</footer>
+       </div>
+       </BrowserRouter>
+     </Provider>
+   );
+ }
 }
 
 export default App;
