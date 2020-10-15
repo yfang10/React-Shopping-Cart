0d0e87b996a8bda2eccf5a4e481437576b6b67cc
AndrewFyy <Yue930315@gmail.com>
2020-10-15 15:44:21 +0800

    Update cartActions.js

--- src/actions/cartActions.js
+++ src/actions/cartActions.js
@@ -28,6 +28,6 @@ export const removeFromCart = (product) => (dispatch, getState) =>{
     const cartItems = getState()
     .cart.cartItems.slice()
     .filter((x) => x._id !== product._id);
-    dispatch({type: REMOVE_FROM, payload:{cartItems}});
+    dispatch({type: REMOVE_FROM_CART, payload:{cartItems}});
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
 };
